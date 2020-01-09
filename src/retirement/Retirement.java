17f088cfed6274c960058f0158a78faacb681fce
高英斐 <gyingfei@linewell.com>
2020-01-09 09:40:03 +0800

    test right

--- src/retirement/Retirement.java
+++ src/retirement/Retirement.java
@@ -86,7 +86,7 @@ public class Retirement {
 				+ ", Unitname=" + Unitname + "]";
 	}
 	public static void main(String[] args) {
-		System.out.println("Test..");
+		System.out.println("Test right..");
 	}
 	
 }
